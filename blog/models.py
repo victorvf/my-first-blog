9a0208bf45af9a2d3ed3869e568995bffdee2c21
victorvf <victorvitoria11@hotmail.com>
2017-09-20 15:56:26 -0300

    My Django app, first commit

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+#models.Model - significa que a class é um modelo de Django, então o Django sabe que ele deve ser salvo no banco de dados
+# author = models.Foreignkey('auth.User')#este é um link para outro modelo
+# title = models.CharField(max_length=200)#assim que você define um texto com um numero limitado de caracteres
+# text = models.TextField()#esse é para textos longos, sem limite.
+# created_date = models.DateTimeField(default=timezone.now)#data e hora
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
